84fa3a47d0879d28a418500747d635ef7043827c
Clément Sibille <clements@lisible.xyz>
2025-07-29 23:38:00 +0200

    Fix colors when rendering illustration

--- src/console.c
+++ src/console.c
@@ -1,27 +1,43 @@
 #include "console.h"
 #include "panic.h"
 
-static char color_char_to_byte(char color) {
-  if (color >= '0' && color <= '9') {
-    return color - '0' + 30;
-  }
+static enum fal_color fal_color_from_char(char color) {
   switch (color) {
+  case '0':
+    return FAL_COLOR_BLACK;
+  case '1':
+    return FAL_COLOR_BLUE;
+  case '2':
+    return FAL_COLOR_GREEN;
+  case '3':
+    return FAL_COLOR_CYAN;
+  case '4':
+    return FAL_COLOR_RED;
+  case '5':
+    return FAL_COLOR_MAGENTA;
+  case '6':
+    return FAL_COLOR_YELLOW;
+  case '7':
+    return FAL_COLOR_WHITE;
+  case '8':
+    return FAL_COLOR_BRIGHTBLACK;
+  case '9':
+    return FAL_COLOR_BRIGHTBLUE;
   case 'A':
-    return 10 + 80;
+    return FAL_COLOR_BRIGHTGREEN;
   case 'B':
-    return 11 + 80;
+    return FAL_COLOR_BRIGHTCYAN;
   case 'C':
-    return 12 + 80;
+    return FAL_COLOR_BRIGHTRED;
   case 'D':
-    return 13 + 80;
+    return FAL_COLOR_BRIGHTMAGENTA;
   case 'E':
-    return 14 + 80;
+    return FAL_COLOR_BRIGHTYELLOW;
   case 'F':
-    return 15 + 80;
+    return FAL_COLOR_BRIGHTWHITE;
+  default:
+    return FAL_COLOR_MAGENTA;
   }
-
-  FAL_PANIC("Invalid color");
-  return 0;
 }
 
 void fal_console_ui_display_illustration(fal_console *console,
@@ -32,10 +48,8 @@ void fal_console_ui_display_illustration(fal_console *console,
     for (int j = 0; j < 80; j++) {
       int x = j;
       fal_console_set_background_color(console,
-                                       color_char_to_byte(illustration[i][j]));
-      fal_console_set_text_color(console,
-                                 color_char_to_byte(illustration[i][j]));
-      fal_console_write_at(console, x, y, &illustration[i][j]);
+                                       fal_color_from_char(illustration[i][j]));
+      fal_console_write_at(console, x, y, " ");
     }
   }
 }
